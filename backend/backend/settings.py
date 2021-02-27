f4a23473a60bb79de4a054c915f44e641f4af740
Justin Reppert <jreppert@gmail.com>
2021-02-26 19:25:26 -0500

    Revert "fix typo in middlware in settings"
    This reverts commit 919fc5577cc787a5947fa629c11f5baaddb58a49.

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware'
+    'corsheaders.middleware.CorsMiddleWare'
 ]
 
 ROOT_URLCONF = 'backend.urls'
