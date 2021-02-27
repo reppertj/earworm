919fc5577cc787a5947fa629c11f5baaddb58a49
Justin Reppert <jreppert@gmail.com>
2021-02-26 19:24:37 -0500

    fix typo in middlware in settings

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleWare'
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'backend.urls'
