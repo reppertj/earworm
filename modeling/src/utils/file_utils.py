3ffbe59810e6db94f3f42eac1af1355744fa4e91
Justin Reppert <jreppert@gmail.com>
2021-03-02 12:06:24 -0500

    add py json deserializer to support training

---
+++ modeling/src/utils/file_utils.py
@@ -0,0 +1,11 @@
+import json
+
+import numpy as np
+import boto3 as aws
+
+
+class NumpyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.ndarray):
+            return obj.tolist()
+        return json.JSONEncoder.default(self, obj)
