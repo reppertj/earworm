5e091f1f6f931e388e6066b8cea8568adc47f9ab
Justin Reppert <jreppert@gmail.com>
2021-03-05 10:44:50 -0500

    refactor initial frontend inference code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,91 +1,156 @@
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
 import axios from "axios";
-import logo from './logo.svg';
-import './App.css';
-import {useDropzone} from 'react-dropzone';
-import { resample } from 'wave-resampler';
-import { Tensor, InferenceSession } from "onnxjs";
-import ndarray from 'ndarray';
-import ops from 'ndarray-ops';
-import r from 'ndarray-concat-rows';
-import prep from './preprocess_model.onnx';
+import "./App.css";
+import { useDropzone } from "react-dropzone";
+import { resample } from "wave-resampler";
+import ndarray from "ndarray";
+import colormap from "colormap";
+import ops from "ndarray-ops";
+import * as tf from "@tensorflow/tfjs";
 
-function getRandomInt(min, max) {
-  min = Math.ceil(min);
+function getRandomInt(max) {
   max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min);
+  return Math.floor(Math.random() * max);
 }
 
-async function preprocess(list_of_ndarrays) {
-  // TODO: Also normalize output before model
+async function resampleAudioBuffer(buffer) {
+  const resampledChannelData = new Array(buffer.numberOfChannels);
+  const offlineCtx = new OfflineAudioContext(
+    buffer.numberOfChannels,
+    buffer.duration * buffer.sampleRate,
+    buffer.sampleRate
+  );
+  const MIN_LENGTH = 10;
+  if (buffer.duration < MIN_LENGTH + 1) {
+    // Give ourselves a little breathing room
+    throw new Error("AudioTooShort");
+  }
+  const TARGET_SAMPLE_RATE = 15950;
+  const TARGET_SAMPLES = TARGET_SAMPLE_RATE * MIN_LENGTH;
+  const start_idx = getRandomInt(
+    TARGET_SAMPLE_RATE * buffer.duration - TARGET_SAMPLES
+  );
+  const end_idx = start_idx + TARGET_SAMPLES;
+  const offlineSource = offlineCtx.createBufferSource();
+  offlineSource.buffer = buffer;
+  offlineSource.connect(offlineCtx.destination);
+  offlineSource.start(0);
+  await offlineCtx.startRendering().then((buffer) => {
+    for (var channel = 0; channel < buffer.numberOfChannels; channel++) {
+      var audioArray = buffer.getChannelData(channel);
+      var resampled = resample(
+        audioArray,
+        buffer.sampleRate,
+        TARGET_SAMPLE_RATE,
+        { method: "sinc" }
+      );
+      resampled = Float32Array.from(resampled);
+      resampledChannelData[channel] = resampled.slice(start_idx, end_idx);
+    }
+  });
+  return resampledChannelData;
+}
 
-  const session = new InferenceSession(); // Initialize early to warmup?
-  const url = prep;
-  session.loadModel(url);
+async function prepareTensor(channelArray) {
+  const inputShape = [channelArray.length, channelArray[0].length];
+  const tensorNd = tf.tensor(channelArray, inputShape);
+  const tensor = tf.mean(tensorNd, 0, true);
+  tf.dispose(tensorNd);
+  return tensor;
+}
 
+async function runInference(tensor) {
+  // Intended for local dev use only
+  const firstUrl = "http://127.0.0.1:8081/waveformSpectrogramModel/model.json";
+  const secondUrl =
+    "http://127.0.0.1:8081/SpectrogramEmbeddingModel/model.json";
+  // TODO: Warmup the model separately to make the first prediction faster
+  const firstModel = await tf.loadGraphModel(firstUrl);
+  const FIRST_SHAPE = [1, 159500];
+  const warmup = await firstModel.executeAsync(
+    { "input:0": tf.zeros(FIRST_SHAPE) },
+    "Identity:0"
+  );
+  tf.dispose(warmup);
+  const sgram = await firstModel.executeAsync(
+    { "input:0": tensor },
+    "Identity:0"
+  );
+  // const secondModel = await tf.loadGraphModel(secondUrl)
+  // const SECOND_SHAPE = [1, 1, 128, 624]
+  // await secondModel.execute({"input_1:0": tf.zeros(SECOND_SHAPE)}, 'Identity:0')
+  return sgram;
+}
+
+async function tensorToImage(tensor) {
+  const max = await tf.max(tensor);
+  const min = await tf.min(tensor);
+  const scaled = await tf.div(tf.sub(tensor, min), tf.sub(max, min));
+  var values = await tf.transpose(scaled).data();
+  const N_SHADES = 72;
+  values = Array.from(values);
+  const colors = colormap({
+    colormap: "jet",
+    nshades: N_SHADES,
+    format: "rba",
+    alpha: 1,
+  });
+  values = values.map((value) => {
+    const index = Math.floor(value * N_SHADES);
+    return colors[index];
+  });
+  let imageData = new Uint8Array();
+  imageData = Uint8Array.from(values.flat());
+  console.log(imageData);
+}
+
+
+function ShowImage(props) {
+  const imageData = props.imageData
+  const canvasRef = React.useRef(null)
+  return (
+    <div>
+      <canvas ref={canvasRef} width={624} height={128} />
+    </div>
+  );
+}
+
+async function runInferenceOnFile(blob) {
+  const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+  const start = new Date();
+  var buffer = await blob
+    .arrayBuffer()
+    .then((arrayBuffer) => audioCtx.decodeAudioData(arrayBuffer));
+  tf.enableDebugMode(); // remove in production
+  const latentVector = await resampleAudioBuffer(buffer)
+    .then((resampled) => prepareTensor(resampled))
+    .then((tensor) => runInference(tensor));
+  await tensorToImage(latentVector);
+  const end = new Date();
+  const inferenceTime = end.getTime() - start.getTime();
+  console.log(latentVector);
+  console.log(inferenceTime);
+  return [buffer, latentVector, inferenceTime];
 }
 
 function Dropzone(props) {
-  const onDrop = useCallback((acceptedFiles) => {
-    acceptedFiles.forEach((file) => {
-      const reader = new FileReader()
-      reader.onabort = () => console.log('file reader was aborted')
-      reader.onerror = () => console.log('file reading failed')
-      reader.onload = (e) => {
-        var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
-        audioCtx.decodeAudioData(e.target.result).then(function(buffer) {
-          var soundSource = audioCtx.createBufferSource();
-          soundSource.buffer = buffer;
-          const sourceSampleRate = buffer.sampleRate;
-          const sourceDuration = buffer.duration;
-          console.log(sourceDuration)
-          // TODO: Reject too short
-          soundSource.connect(audioCtx.destination);
-          soundSource.start(0);
-          console.log(sourceSampleRate)
-          const TARGET_SAMPLE_RATE = 15950;
-          const offlineCtx = new OfflineAudioContext(buffer.numberOfChannels, buffer.duration * buffer.sampleRate, buffer.sampleRate);
-          const cloneBuffer = offlineCtx.createBuffer(buffer.numberOfChannels, buffer.length, buffer.sampleRate);
-          for (let channel = 0; channel < buffer.numberOfChannels; channel++) {
-            cloneBuffer.copyToChannel(buffer.getChannelData(channel), channel);
-          }
-          var offlineSource = offlineCtx.createBufferSource();
-          offlineSource.buffer = cloneBuffer;
-          offlineSource.connect(offlineCtx.destination);
-          offlineCtx.oncomplete = function(e) {
-            const resampledAudioBuffer = e.renderedBuffer;
-            console.log(resampledAudioBuffer.getChannelData(0).length)
-            var resampledAudioArrays = []
-            for (let channel = 0; channel < resampledAudioBuffer.numberOfChannels; channel++) {
-              let audioArray = resampledAudioBuffer.getChannelData(channel);
-              let resampled = resample(audioArray, sourceSampleRate, TARGET_SAMPLE_RATE, {method: "sinc"});
-              console.log(resampled);
-              var resampled_ndarray = ndarray(resampled, [1, resampled.length])
-              resampledAudioArrays.push(resampled_ndarray);
-            }
-            var ndarray_waveform = r(resampledAudioArrays)
-            preprocess(ndarray_waveform)
-          }
-          offlineCtx.startRendering();
-          offlineSource.start(0);
-        });
-      }
-      reader.readAsArrayBuffer(file);
-  })
-}, [])
+  const onDrop = useCallback(
+    (acceptedFiles) => runInferenceOnFile(acceptedFiles[0]),
+    []
+  );
 
-const {
-  acceptedFiles,
-  fileRejections,
-  getRootProps,
-  getInputProps
-} = useDropzone({
-  accept: 'audio/*',
-  onDrop,
-  maxFiles:1
-});
+  const {
+    acceptedFiles,
+    fileRejections,
+    getRootProps,
+    getInputProps,
+  } = useDropzone({
+    accept: "audio/*",
+    onDrop,
+    maxFiles: 1,
+  });
 
-  const acceptedFileItems = acceptedFiles.map(file => (
+  const acceptedFileItems = acceptedFiles.map((file) => (
     <li key={file.path}>
       {file.path} - {file.size} bytes
     </li>
@@ -95,7 +160,7 @@ const {
     <li key={file.path}>
       {file.path} - {file.size} bytes
       <ul>
-        {errors.map(e => (
+        {errors.map((e) => (
           <li key={e.code}>{e.message}</li>
         ))}
       </ul>
@@ -104,7 +169,7 @@ const {
 
   return (
     <section className="container">
-      <div {...getRootProps({ className: 'dropzone' })}>
+      <div {...getRootProps({ className: "dropzone" })}>
         <input {...getInputProps()} />
         <p>Drag and drop an audio file here, or click to select a file.</p>
       </div>
@@ -118,15 +183,35 @@ const {
   );
 }
 
-function App (props) {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <Dropzone />
-        </header>
-      </div>
-    );  
- 
+function SearchBar(props) {
+  return (
+    <Dropzone />
+  );
 }
 
-export default App;
+function SearchInfo(props) {
+  return (
+    "Hello"
+  );
+}
+
+function NeighborSearch(props) {
+  return (
+    <div>
+      <SearchBar />
+      <SearchInfo />
+    </div>
+  );
+}
+
+function App(props) {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <NeighborSearch />
+      </header>
+    </div>
+  );
+}
+
+export { runInferenceOnFile }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,11 +6,12 @@ import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 import App from './App';
+import Layout from './layout';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Layout />
   </React.StrictMode>,
   document.getElementById('root')
 );
