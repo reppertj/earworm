0fa7acf87d99f339a8ea9d466d1b271f6e82441b
Justin Reppert <jreppert@gmail.com>
2021-03-10 08:29:57 -0500

    tweak log-scale formulation for nicer amplitude distributions

--- modeling/src/data/preprocessing.py
+++ modeling/src/data/preprocessing.py
@@ -113,10 +113,12 @@ class AudioTooShortError(ValueError):
 
 
 class TensorPreprocesser:
-    def __init__(self) -> None:
-        self.model = WaveformToDBSpectrogram()
-    
-    def __call__(self, in_paths: List[str], out_paths: List[str], mfcc=False, n_workers=2) -> None:        
+    def __init__(self, model) -> None:
+        self.model = model
+
+    def __call__(
+        self, in_paths: List[str], out_paths: List[str], mfcc=False, n_workers=2
+    ) -> Tuple[List[str], List[str]]:
         def write_out(inp, out):
             try:
                 tensor = self.model.from_path(inp, mfcc)
@@ -129,72 +131,35 @@ class TensorPreprocesser:
             except:
                 print("Failure:", out)
                 return (out, False)
-                            
+
         with ThreadPool(nodes=n_workers) as P:
             results = P.imap(write_out, in_paths, out_paths)
         successes = [path for path, res in results if res]
         failures = [path for path, res in results if not res]
         return successes, failures
-    
-
-class JSSupportedMelSpectrogram(nn.Module):
-    def __init__(
-        self,
-        sample_rate: int = 15950,
-        seconds: float = 10.0,
-        win_length: int = 1024,
-        hop_length: int = 256,
-        n_mels: int = 128,
-        validate_target: Tuple[int] = (1, 128, 624),
-    ):
-        super().__init__()
-        self.sample_rate = sample_rate
-        self.seconds = seconds
-        self.min_samples = int(sample_rate * seconds)
-        self.win_length = win_length
-        self.hop_length = hop_length
-        self.n_mels = n_mels
-        self.validate_target = validate_target
-        self.input_sample = torch.randn(
-            (1, int(self.seconds * self.sample_rate)), dtype=torch.float32
-        )
-        self.mel_spectogram = nn.Sequential(
-            torchlibrosa.Spectrogram(
-                n_fft=self.win_length,
-                hop_length=self.hop_length,
-                win_length=self.win_length,
-            ),
-            torchlibrosa.LogmelFilterBank(
-                sr=self.sample_rate,
-                n_fft=self.win_length,
-                n_mels=self.n_mels,
-            ),
-        )
-
-    def forward(self, waveform):
-        sgram = self.mel_spectogram(waveform).squeeze(0).transpose(1, 2)
-        return sgram
 
 
 class DoubleSqueeze(nn.Module):
     def __init__(self):
         super().__init__()
-    
+
     def forward(self, input):
         return input.squeeze().squeeze()
+
 
 class WaveformToDBSpectrogram(nn.Module):
     def __init__(
         self,
-        sample_rate: int = 15950,
-        seconds: float = 10.0,
+        sample_rate: int = 22050,
+        seconds: float = 3.0,
         win_length: int = 1024,
-        hop_length: int = 256,
+        hop_length: int = 512,
         n_mels: int = 128,
-        validate_target: Tuple[int] = (1, 128, 624),
+        validate_target: Tuple[int, int] = (128, 130),
     ):
         """
-        Yields dB-scaled mel spectrograms from random crop of length `seconds` from audio files.
+        Returns log10 (not dB) scaled mel spectrograms from random crop of length `seconds` from
+        audio files.
         Output size depends on parameters; pass shape to `validate_target` to raise an error if the
         output tensor does not match. Spectrograms are NOT standardized for friendlier gradients;
         this should be done by the relevant dataset class or inference code.
@@ -213,6 +178,7 @@ class WaveformToDBSpectrogram(nn.Module):
         self.mel_spectogram = nn.Sequential(
             torchlibrosa.Spectrogram(
                 n_fft=self.win_length,
+                center=True,
                 hop_length=self.hop_length,
                 win_length=self.win_length,
             ),
@@ -240,12 +206,17 @@ class WaveformToDBSpectrogram(nn.Module):
         """
         Input: (1, self.sample_rate * self.seconds)
         Output: self.validate_target
-        This is intended mainly for use in the frontend through export to tf.js 
+        This is intended mainly for use in the frontend through export to tf.js
         """
-        sgram = self.mel_spectogram(waveform)
-        return torch.log(torch.clamp(sgram, min=1e-10)).div(
-            torch.log(torch.tensor(10.0))
-        )
+        sgram = self.mel_spectogram(waveform).t()
+        if self.validate_target:
+            assert sgram.shape == self.validate_target
+        log_spec = torch.log(torch.clamp(sgram, min=1e-10)).div(torch.log(torch.tensor(10.)))
+        return log_spec
+        # return torch.log(torch.clamp(sgram, min=1e-10)).div(
+        #     torch.log(torch.tensor(10.0))
+        # )
+        # return torch.log10(1 + sgram * 10)
 
     def get_mfcc(self, waveform: torch.Tensor):
         melkwargs = {
@@ -259,16 +230,18 @@ class WaveformToDBSpectrogram(nn.Module):
         stds = coefs.std(dim=2)
         return torch.cat((means, stds), dim=1)
 
-    def from_path(self, path: Iterable[str], return_mfcc=False):
+    def from_path(self, path: str, return_mfcc=False):
         logger.info(f"Processing {path} to tensor of spectrogram")
         waveform, inp_freq = torchaudio.load(path)
         waveform = waveform.mean(dim=0, keepdims=True)
         waveform = Resample(inp_freq, self.sample_rate)(waveform)
         n_samples = waveform.shape[1]
         if n_samples < self.min_samples:
-            raise AudioTooShortError(f"Input must be at least {self.seconds} seconds long")
+            raise AudioTooShortError(
+                f"Input must be at least {self.seconds} seconds long"
+            )
         start_idx = torch.randint(0, n_samples - self.min_samples, (1,))
-        waveform = waveform[:, start_idx : (start_idx + self.min_samples)]
+        waveform = waveform[:, start_idx : (start_idx + self.min_samples)]  # type: ignore
         sgram = self.forward(waveform)
         if return_mfcc:
             return sgram, self.get_mfcc(waveform)
