53cff6569b14aa0203e16a2591094fe9475c59a6
Justin Reppert <jreppert@gmail.com>
2021-03-26 06:55:33 -0400

    add assertions to test fixtures to fail when needed environmental variables don't exist

--- modeling/music_metric_learning/conftest.py
+++ modeling/music_metric_learning/conftest.py
@@ -6,18 +6,22 @@ assert load_dotenv(dotenv_path="local.modeling.env") == True
 
 @pytest.fixture(scope="session")
 def tensor_dir():
+    assert os.getenv("YT_TENSOR_DATA_DIR") is not None
     return os.getenv("YT_TENSOR_DATA_DIR")
 
 @pytest.fixture(scope="session")
 def audio_dir():
+    assert os.getenv("YT_AUDIO_DATA_DIR") is not None
     return os.getenv("YT_AUDIO_DATA_DIR")
 
 @pytest.fixture(scope="session")
 def audioset_json():
+    assert os.getenv("AUDIOSET_JSON_PATH") is not None
     return os.getenv("AUDIOSET_JSON_PATH")
 
 @pytest.fixture(scope="session")
 def audioset_csv():
+    assert os.getenv("AUDIOSET_TRAIN_CSV_PATH") is not None
     return os.getenv(
         "AUDIOSET_TRAIN_CSV_PATH"
     )
