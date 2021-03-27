4ef3de7b8bc323c3c430211d5520443e20fc9a3b
Justin Reppert <jreppert@gmail.com>
2021-03-26 20:36:57 -0400

    add MoCo batch shuffle functions

--- modeling/music_metric_learning/utils/model_utils.py
+++ modeling/music_metric_learning/utils/model_utils.py
@@ -197,3 +197,22 @@ def copy_parameters(
             query_encoder.parameters(), key_enconder.parameters()
         ):
             param_k.data = param_k.data * momentum + param_q.data * (1.0 - momentum)
+
+
+def batch_shuffle_single_gpu(x):
+    """
+    Batch shuffle, for making use of BatchNorm on a single gpu to avoid 
+    "leaking" information between keys and queries.
+    Taken from: https://github.com/KevinMusgrave/pytorch-metric-learning/blob/master/examples/notebooks/MoCoCIFAR10.ipynb
+    """
+    # random shuffle index
+    idx_shuffle = torch.randperm(x.shape[0]).cuda()
+    # index for restoring
+    idx_unshuffle = torch.argsort(idx_shuffle)
+    return x[idx_shuffle], idx_unshuffle
+
+def batch_unshuffle_single_gpu(x, idx_unshuffle):
+    """
+    Undo batch shuffle.
+    """
+    return x[idx_unshuffle]
