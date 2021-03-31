44dd64e7be05ecfb8d488286e32800e8bc878262
Justin Reppert <jreppert@gmail.com>
2021-03-31 15:28:00 -0400

    fix training sanity tests

--- modeling/music_metric_learning/tests/test_models_train.py
+++ modeling/music_metric_learning/tests/test_models_train.py
@@ -1,5 +1,6 @@
 import pytest
 import torch
+from music_metric_learning.losses.cross_entropy import MoCoCrossEntropyLoss
 from music_metric_learning.models.train_music_learner import MusicMetricLearner, DEFAULT_HPARAMS
 from pytorch_lightning import Trainer
 
@@ -10,26 +11,42 @@ they don't check for correctness or performance.
 def test_trainer_config(tmpdir, datamodule):
     hparams = DEFAULT_HPARAMS
     hparams["batch_size"] = 16
+    hparams["m_per_class"] = 2
     hparams["checkpoint_path"] = str(tmpdir)
-    learner = MusicMetricLearner(datamodule=datamodule, hparams=hparams)
+    learner = MusicMetricLearner(datamodule=datamodule, conf=hparams)
     trainer = Trainer(gpus=0, fast_dev_run=True)
     trainer.fit(learner)
 
 def test_track_loss_only(tmpdir, datamodule):
     hparams = DEFAULT_HPARAMS
     hparams["batch_size"] = 16
+    hparams["m_per_class"] = 2
     hparams["checkpoint_path"] = str(tmpdir)
     hparams["mode"] = "track"
-    learner = MusicMetricLearner(datamodule=datamodule, hparams=hparams)
+    learner = MusicMetricLearner(datamodule=datamodule, conf=hparams)
+    trainer = Trainer(gpus=0, fast_dev_run=True)
+    trainer.fit(learner)
+
+def test_train_with_moco_loss(tmpdir, datamodule):
+    hparams = DEFAULT_HPARAMS
+    hparams["batch_size"] = 16
+    hparams["m_per_class"] = 2
+    hparams["checkpoint_path"] = str(tmpdir)    
+    hparams["loss_func"] = "moco"
+    hparams["loss_params"]["temperature"] = 0.7
+    learner = MusicMetricLearner(datamodule=datamodule, conf=hparams)
+    assert isinstance(learner.criterion, MoCoCrossEntropyLoss)
+    assert learner.criterion.temperature == 0.7
     trainer = Trainer(gpus=0, fast_dev_run=True)
     trainer.fit(learner)
 
 def test_inception_encoder(tmpdir, datamodule):
     hparams = DEFAULT_HPARAMS
     hparams["batch_size"] = 16
+    hparams["m_per_class"] = 2
     hparams["checkpoint_path"] = str(tmpdir)
     hparams["encoder"] = "inception"
-    learner = MusicMetricLearner(datamodule=datamodule, hparams=hparams)
+    learner = MusicMetricLearner(datamodule=datamodule, conf=hparams)
     trainer = Trainer(gpus=0, fast_dev_run=True)
     trainer.fit(learner)    
     
@@ -38,7 +55,8 @@ def test_inception_encoder(tmpdir, datamodule):
 def test_mixed_precision(tmpdir, datamodule):
     hparams = DEFAULT_HPARAMS
     hparams["batch_size"] = 16
+    hparams["m_per_class"] = 2
     hparams["checkpoint_path"] = str(tmpdir)
-    learner = MusicMetricLearner(datamodule=datamodule, hparams=hparams)
+    learner = MusicMetricLearner(datamodule=datamodule, conf=hparams)
     trainer = Trainer(gpus=1, precision=16, fast_dev_run=True)
     trainer.fit(learner)
