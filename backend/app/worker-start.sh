456220dc0792702c410ea2d801f63657dc78859f
Justin Reppert <jreppert@gmail.com>
2021-04-13 19:52:15 -0400

    improve development experience with worker image by using live reloading in dev

--- backend/app/worker-start.sh
+++ backend/app/worker-start.sh
@@ -3,4 +3,9 @@ set -e
 
 python /app/app/celeryworker_pre_start.py
 
-celery -A app.worker worker --loglevel=INFO -Q main-queue
+if [ "$RUN" == "" ]
+  then
+    celery -A app.worker worker --loglevel=INFO -Q main-queue
+  else
+    eval $RUN
+fi
