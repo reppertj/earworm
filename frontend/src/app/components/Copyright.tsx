d1de695c5b69e389a650ec135fa3ed1c1ff1c200
Justin Reppert <10255098+reppertj@users.noreply.github.com>
2021-04-23 05:15:27 -0400

    open footer link in new window

--- frontend/src/app/components/Copyright.tsx
+++ frontend/src/app/components/Copyright.tsx
@@ -6,7 +6,12 @@ export default function Copyright(props) {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'By '}
-      <Link color="inherit" href="https://www.justinreppert.com/">
+      <Link
+        color="inherit"
+        target="_blank"
+        rel="noopner"
+        href="https://www.justinreppert.com/"
+      >
         Justin Reppert
       </Link>{' '}
       {new Date().getFullYear()}
